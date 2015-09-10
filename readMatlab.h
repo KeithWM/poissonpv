d67d3d072e67ecce8be317000310a6868ee65f95
Keith <keith@dhcp-10-33-225-68.icts.kuleuven.be>
2015-09-10 09:30:33 +0200

    cleared the (unused) repository in order to add new code

--- readMatlab.h
+++
@@ -1,9 +0,0 @@
-#ifndef READMATLAB_H
-#define READMATLAB_H
-
-#include "parallel.h"
-
-int readInitCondFromMatLab(struct Solution *psol, struct Dynamics *pdyn, struct Parameters par, char *file);
-int readOrderingFromMatLab(struct Dynamics *pdyn, char *file);
-#endif
-
